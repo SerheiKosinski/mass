2915381ab3f4dd56fc0b158db2e3a7ac97d76ddd
SerheiKosinski <107434353+SerheiKosinski@users.noreply.github.com>
2022-07-19 23:53:41 +0300

    Add files via upload

---
+++ src/by/epam/tr/mas1/mas03.java
@@ -0,0 +1,37 @@
+package by.epam.tr.mas1;
+
+import java.util.Scanner;
+
+public class mas03 {
+
+	public static void main(String[] args) {
+		// ƒана последовательность целых чисел а1 а2,..., аn . ¬ы€снить, какое число встречаетс€ раньше - положительное или
+		//отрицательное
+		
+		
+		int n = 4;
+		int[] mas = new int[n];
+		
+		Scanner sc = new Scanner(System.in);
+		
+		   for (int i = 0; i < mas.length; i++) {
+		    System.out.print("mas[" + i + "]=");
+		      while (!sc.hasNextInt()) {
+		        sc.nextLine();
+		        System.out.print("mas[" + i + "]=");
+		  }
+		      
+		mas[i] = sc.nextInt();
+		
+		}
+		
+	       
+				      if (mas[0] > 0) {
+				    	  System.out.println("раьше положительное");
+				      } else {
+				    	  System.out.println("раньше отрицательное");
+				      }
+				   
+	}
+
+}
